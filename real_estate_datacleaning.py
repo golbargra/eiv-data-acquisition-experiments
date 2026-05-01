0927e9548e66b2244e0d87404aa98015158f3ffb
golbargra <golbargroghani@gmail.com>
2026-05-01 18:39:00 -0400

    few changes

--- real_estate_datacleaning.py
+++ real_estate_datacleaning.py
@@ -112,7 +112,6 @@ print(df.head())
 print("\nMissing values:")
 print(df.isna().sum().sort_values(ascending=False).head(20))
 
-print("\nBasic statistics:")
+print("\nBasic statisticss:")
 print(df.describe().T.head(30))
 
-print("\nSaved as: real_estate_clean_merged.csv")
